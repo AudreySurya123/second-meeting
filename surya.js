73948e93d4bbc006d336a24b987b8e2d6d94a699
AudreySurya123 <127375983+AudreySurya123@users.noreply.github.com>
2023-05-26 23:44:11 +0700

    surya.js

---
+++ surya.js
@@ -0,0 +1,10 @@
+function fullName(name, nim) {
+    const mahasiswa = {
+      name: name,
+      nim: nim
+    };
+  
+    console.log(mahasiswa);
+  }
+  
+  fullName("Audrey Surya Nanditama Hernanto", "V3922008");
